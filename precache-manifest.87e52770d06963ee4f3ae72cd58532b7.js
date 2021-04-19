191626d32dc46b2991f7b0e6e30f510c20b40d17
Chintan-K <kalsariachintan@gmail.com>
2021-04-19 20:34:50 +0530

    Updates

---
+++ precache-manifest.87e52770d06963ee4f3ae72cd58532b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2420df13c41708ec9b43cdd9b8a89a02",
+    "url": "/covid-statistics/index.html"
+  },
+  {
+    "revision": "47ea3b075ea03d120dc9",
+    "url": "/covid-statistics/static/css/main.46f1b7e7.chunk.css"
+  },
+  {
+    "revision": "a76e753f82c4c208b865",
+    "url": "/covid-statistics/static/js/2.2684ed6c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covid-statistics/static/js/2.2684ed6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ea3b075ea03d120dc9",
+    "url": "/covid-statistics/static/js/main.3638a380.chunk.js"
+  },
+  {
+    "revision": "212396e409d5c63befe4",
+    "url": "/covid-statistics/static/js/runtime-main.ba201014.js"
+  }
+]);
